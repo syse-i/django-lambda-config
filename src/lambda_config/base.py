939314a8bdbc01ac739f63c82e676a164e422342
David Sosa Valdes <david.sosa.valdes@gmail.com>
2021-08-15 16:11:53 -0500

    First commit on new version

--- src/lambda_config/base.py
+++ src/lambda_config/base.py
@@ -1,207 +1,311 @@
-import os
+"""
+DOCSTRING
+"""
 
-from django.utils.translation import gettext as _
+import inspect
+from sys import modules as sys_modules
+from pathlib import Path
+from dataclasses import dataclass, field
+from typing import List, Callable, Union, Optional
 
-from . import Settings as CoreSettings
-from .helpers import base_dir_path
+# noinspection PyPackageRequirements
+from decouple import config
 
-__all__ = [
-    'Settings',
-    'BaseDir',
+__all__ = ['Settings']
+
+ListStr = List[str]
+OptionalListAttr = Union[ListStr, Callable[[ListStr], ListStr], None]
+
+
+def parse_liststr_attr(attr: OptionalListAttr, default: ListStr) -> ListStr:
+    """
+    DOCSTRING
+    """
+    if callable(attr):
+        return attr(default)
+    if isinstance(attr, list):
+        return default + attr
+    if attr is None:
+        return default
+    return attr
+
+
+def parent_path(file_path, depth, n=0):
+    """
+    DOCSTRING
+    """
+    return parent_path(file_path.parent, depth, n + 1) if n < depth else file_path.parent
+
+
+def base_dir_path(*args, depth=2):
+    """
+    DOCSTRING
+    """
+    return str(parent_path(Path(*args).resolve(), depth))
+
+
+# APPS definition
+__INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
-BaseDir = base_dir_path
+__MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+__AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+__AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+__ALLOWED_HOSTS = []
+
+PARSE_DEFAULT_ATTRS = [
+    ('INSTALLED_APPS', parse_liststr_attr, __INSTALLED_APPS),
+    ('MIDDLEWARE', parse_liststr_attr, __MIDDLEWARE),
+    ('AUTHENTICATION_BACKENDS', parse_liststr_attr, __AUTHENTICATION_BACKENDS),
+    ('AUTH_PASSWORD_VALIDATORS', parse_liststr_attr, __AUTH_PASSWORD_VALIDATORS),
+    ('ALLOWED_HOSTS', parse_liststr_attr, __ALLOWED_HOSTS),
+]
 
 
-# noinspection PyPep8Naming
-class Settings(CoreSettings):
+# @dataclass
+# class Template:
+#     """
+#     DOCSTRING
+#     """
+#     BACKEND: str = 'django.template.backends.django.DjangoTemplates'
+#     DIRS: List[str] = field(default_factory=list)
+#     APP_DIRS: bool = True
+#     OPTIONS: Union[dict, Callable[[dict], dict], None] = None
+#
+#     @staticmethod
+#     def get_default_options():
+#         """
+#         DOCSTRING
+#         """
+#         return {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ]
+#         }
+#
+#     def __post_init__(self):
+#         default_options = self.get_default_options()
+#         if callable(self.OPTIONS):
+#             options = self.OPTIONS(default_options)
+#         elif isinstance(self.OPTIONS, dict):
+#             options = default_options
+#             options.update(self.OPTIONS)
+#         else:
+#             options = None
+#         self.OPTIONS = default_options if options is None else options
+#
+#
+# ListTemplate = List[Template]
+# OptionalListTemplate = Union[ListTemplate, Callable[[ListTemplate], ListTemplate], None]
+
+
+# ('CONTEXT_PROCESSORS', parse_liststr_attr, __CONTEXT_PROCESSORS),
+
+# {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(self.BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# @dataclass
+# class DATABASE:
+#     tag_name: str
+#     ENGINE: str
+#     NAME: Path
+#
+#     def __post_init__(self):
+#         pass
+
+
+@dataclass
+class Settings:
     """
-    Lambda Core base settings, don't use this directly.
+    Class based settings for complex settings inheritance.
     """
 
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-    BASE_DIR = os.environ.get('BASE_DIR')
+    BASE_DIR: str
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY: str = config('SECRET_KEY')
 
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
+    DEBUG: bool = config('DEBUG', default=True)
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS: OptionalListAttr = None
 
-    # Application definition
+    INSTALLED_APPS: OptionalListAttr = None
 
-    _INSTALLED_APPS = [
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-    ]
+    MIDDLEWARE: OptionalListAttr = None
 
-    @property
-    def INSTALLED_APPS(self):
-        return self._INSTALLED_APPS
-
-    @INSTALLED_APPS.setter
-    def INSTALLED_APPS(self, values):
-        self._INSTALLED_APPS = values
-
-    _MIDDLEWARE = [
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ]
-
-    @property
-    def MIDDLEWARE(self):
-        return self._MIDDLEWARE
-
-    @MIDDLEWARE.setter
-    def MIDDLEWARE(self, values):
-        self._MIDDLEWARE = values
-
-    ROOT_URLCONF = os.environ.get('ROOT_URLCONF', 'config.urls')
-
-    _CONTEXT_PROCESSORS = [
-        'django.template.context_processors.debug',
-        'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages',
-    ]
-
-    @property
-    def CONTEXT_PROCESSORS(self):
-        return self._CONTEXT_PROCESSORS
-
-    @CONTEXT_PROCESSORS.setter
-    def CONTEXT_PROCESSORS(self, values):
-        self._CONTEXT_PROCESSORS = values
-
-    _TEMPLATES_DIRS = []
-
-    @property
-    def TEMPLATES_DIRS(self):
-        return self._TEMPLATES_DIRS
-
-    @TEMPLATES_DIRS.setter
-    def TEMPLATES_DIRS(self, values):
-        self._TEMPLATES_DIRS = values
+    ROOT_URLCONF: str = config('ROOT_URLCONF', default='config.urls')
 
     # noinspection PyPropertyAccess
-    @property
-    def TEMPLATES(self):
-        return [{
+    # @property
+    # def TEMPLATES(self):
+    #     return [{
+    #         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    #         'DIRS': self.TEMPLATES_DIRS,
+    #         'APP_DIRS': True,
+    #         'OPTIONS': {
+    #             'context_processors': self.CONTEXT_PROCESSORS,
+    #         },
+    #     }]
+
+    TEMPLATES: List[dict] = field(default_factory=lambda: [
+        {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': self.TEMPLATES_DIRS,
+            'DIRS': [],
             'APP_DIRS': True,
             'OPTIONS': {
-                'context_processors': self.CONTEXT_PROCESSORS,
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
             },
-        }]
+        }
+    ])
 
-    WSGI_APPLICATION = os.environ.get('WSGI_APPLICATION', 'config.wsgi.application')
+    WSGI_APPLICATION: str = config('WSGI_APPLICATION', default='config.wsgi.application')
 
     # Database
     # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-    @property
-    def DATABASES(self):
-        return {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(self.BASE_DIR, 'db.sqlite3'),
-            }
+    DATABASES: dict = field(default_factory=lambda: {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+            # 'NAME': os.path.join(self.BASE_DIR, 'db.sqlite3'),
         }
+    })
 
     # Password validation
     # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
+    AUTH_PASSWORD_VALIDATORS: OptionalListAttr = None
 
     # Authentication
     # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/
 
-    _AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
-
-    @property
-    def AUTHENTICATION_BACKENDS(self):
-        return self._AUTHENTICATION_BACKENDS
-
-    @AUTHENTICATION_BACKENDS.setter
-    def AUTHENTICATION_BACKENDS(self, values):
-        self._AUTHENTICATION_BACKENDS = values
+    AUTHENTICATION_BACKENDS: OptionalListAttr = None
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-    LANGUAGE_CODE = os.environ.get('LANGUAGE_CODE', 'en-US')
+    LANGUAGE_CODE: str = config('LANGUAGE_CODE', default='en-US')
 
-    TIME_ZONE = os.environ.get('TIME_ZONE', 'UTC')
+    TIME_ZONE: str = config('TIME_ZONE', default='UTC')
 
-    USE_I18N = True
+    USE_I18N: bool = True
 
-    USE_L10N = True
+    USE_L10N: bool = True
 
-    USE_TZ = True
+    USE_TZ: bool = True
 
-    _LOCALE_PATHS = []
-
-    @property
-    def LOCALE_PATHS(self):
-        return self._LOCALE_PATHS
-
-    @LOCALE_PATHS.setter
-    def LOCALE_PATHS(self, values):
-        self._LOCALE_PATHS = values
+    LOCALE_PATHS: List[Path] = field(default_factory=list)
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-    STATIC_URL = os.environ.get('STATIC_URL', '/static/')
+    STATIC_URL: str = config('STATIC_URL', default='/static/')
 
-    @property
-    def STATIC_ROOT(self):
-        return os.path.join(self.BASE_DIR, 'static/')
+    STATIC_ROOT: str = ''
 
-    _STATICFILES_DIRS = []
+    # @property
+    # def STATIC_ROOT(self):
+    #     return os.path.join(self.BASE_DIR, 'static/')
 
-    @property
-    def STATICFILES_DIRS(self):
-        return self._STATICFILES_DIRS
+    STATICFILES_DIRS: List[Path] = field(default_factory=list)
 
-    @STATICFILES_DIRS.setter
-    def STATICFILES_DIRS(self, values):
-        self._STATICFILES_DIRS = values
+    # @property
+    # def STATICFILES_DIRS(self):
+    #     return self._STATICFILES_DIRS
+    #
+    # @STATICFILES_DIRS.setter
+    # def STATICFILES_DIRS(self, values):
+    #     self._STATICFILES_DIRS = values
 
-    MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
+    MEDIA_URL: str = config('MEDIA_URL', default='/media/')
 
-    @property
-    def MEDIA_ROOT(self):
-        return os.path.join(self.BASE_DIR, 'media/')
+    MEDIA_ROOT: str = ''
 
-    def __init__(self, *args, **kwargs):
-        if not self.BASE_DIR:
-            raise Exception(f'{_("BASE_DIR must be defined...")}')
+    # @property
+    # def MEDIA_ROOT(self):
+    #     return os.path.join(self.BASE_DIR, 'media/')
+
+    ###########################################################################
+    #                                                                         #
+    ###########################################################################
+
+    def __post_init__(self):
+        for attr_name, parser, default in PARSE_DEFAULT_ATTRS:
+            attr = getattr(self, attr_name)
+            setattr(self, attr_name, parser(attr, default))
+
+    ###########################################################################
+    #                                                                         #
+    ###########################################################################
+
+    def load(self, module_name):
+        """
+        Export class variables and properties to module namespace.
+
+        This will export and class variable that is all upper case and doesn't
+        begin with ``_``. These members will be set as attributes on the module
+        ``module_name``.
+        """
+        module = sys_modules[module_name]
+        for (member, value) in inspect.getmembers(self):
+            if member.isupper() and not member.startswith('_'):
+                if isinstance(value, property):
+                    # noinspection PyArgumentList
+                    value = value.fget(self)
+                setattr(module, member, value)
+
+
+if __name__ == "__main__":
+    x = Settings(__file__)
+    x.load(__name__)
+    # noinspection PyUnresolvedReferences
+    print(x, INSTALLED_APPS)
